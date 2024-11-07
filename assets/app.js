29228159aa4ff801952d47977b0c556f1b8b6772
najoro Fanantenana <najofanantenana@gmail.com>
2024-11-07 14:19:38 +0100

    organization of places in the bus

--- assets/app.js
+++ assets/app.js
@@ -9,15 +9,15 @@ $(document).ready(function () {
   var data = [
     {
       id: 1,
-      date: "7h - 15h",
-      hotel: "Carlton",
+      heure: "7h - 15h",
       nom: "Ando",
-      adresse: "23 rue de l’Indépendance",
+      hotel: "Carlton",
+      adresse: "23 rue de l'Indépendance",
       contact: "0345678901",
     },
     {
       id: 2,
-      date: "8h - 14h",
+      heure: "8h - 14h",
       hotel: "Ibis",
       nom: "Haja",
       adresse: "56 rue Razanamihaja",
@@ -25,7 +25,7 @@ $(document).ready(function () {
     },
     {
       id: 3,
-      date: "9h - 13h",
+      heure: "9h - 13h",
       hotel: "Hilton",
       nom: "Fara",
       adresse: "10 avenue de l’Université",
@@ -33,7 +33,7 @@ $(document).ready(function () {
     },
     {
       id: 4,
-      date: "11h - 12h",
+      heure: "11h - 12h",
       hotel: "Le Louvre",
       nom: "Voara",
       adresse: "78 rue d’Ambohitsirohitra",
@@ -41,7 +41,7 @@ $(document).ready(function () {
     },
     {
       id: 5,
-      date: "12h - 13h",
+      heure: "12h - 13h",
       hotel: "Carlton",
       nom: "Tahina",
       adresse: "34 rue du Commerce",
@@ -49,7 +49,7 @@ $(document).ready(function () {
     },
     {
       id: 6,
-      date: "16h - 18h",
+      heure: "16h - 18h",
       hotel: "Ibis",
       nom: "Solo",
       adresse: "12 rue de Mahamasina",
@@ -57,7 +57,7 @@ $(document).ready(function () {
     },
     {
       id: 7,
-      date: "17h - 19h",
+      heure: "17h - 19h",
       hotel: "Hilton",
       nom: "Lova",
       adresse: "89 avenue de l’Amitié",
@@ -65,7 +65,7 @@ $(document).ready(function () {
     },
     {
       id: 8,
-      date: "17h - 19h",
+      heure: "17h - 19h",
       hotel: "Hilton",
       nom: "Lova",
       adresse: "89 avenue de l’Amitié",
@@ -73,7 +73,7 @@ $(document).ready(function () {
     },
     {
       id: 9,
-      date: "17h - 19h",
+      heure: "17h - 19h",
       hotel: "Hilton",
       nom: "Lova",
       adresse: "89 avenue de l’Amitié",
@@ -81,7 +81,7 @@ $(document).ready(function () {
     },
     {
       id: 10,
-      date: "17h - 19h",
+      heure: "17h - 19h",
       hotel: "Hilton",
       nom: "Lova",
       adresse: "89 avenue de l’Amitié",
@@ -89,45 +89,46 @@ $(document).ready(function () {
     },
   ];
 
-  var dataInCar = {
+  var dataCar = {
     voiture: "Voiture 1",
     trajet: "Analakely- Mahazo",
-    data: {
-      chauffeur: { id: 1, name: "Paul", adresse: "67ha", point: "106" },
-      passage: [
-        {key: 1, personnel: { id: 1, date: "7h - 15h", hotel: "Carlton", nom: "Ando", adresse: "23 rue de l’Indépendance",contact: "0345678901",}},
-        {key: 2, personnel: { id: 3, date: "7h - 15h", hotel: "Carlton", nom: "Lova", adresse: "23 rue de l’Indépendance",contact: "0345678901",}},
-        {key: 3, personnel: { id: 4, date: "7h - 15h", hotel: "Carlton", nom: "koko", adresse: "23 rue de l’Indépendance",contact: "0345678901",}},
-        {key: 4, personnel: ""},
-        {key: 5, personnel: ""},
-        {key: 6, personnel: ""},
-        {key: 7, personnel: ""},
-        {key: 8, personnel: ""},
-        {key: 9, personnel: ""},
-        {key: 10, personnel: ""},
-        {key: 11, personnel: ""},
-        {key: 12, personnel: ""},
-        {key: 13, personnel: ""},
-        {key: 14, personnel: ""},
-        {key: 15, personnel: ""},
-        {key: 16, personnel: ""},
-        {key: 17, personnel: ""},
-        {key: 18, personnel: ""},
-        {key: 19, personnel: ""},
-        {key: 20, personnel: ""},
-        {key: 21, personnel: ""},
-        {key: 22, personnel: ""},
-      ]
-    },
+    data: [
+      {id: 'c', personnel: { id: 1, name: "Vonjy", adresse: "67ha", point: "106" }},
+      {id: 'p1', personnel: { id: 1, date: "7h - 15h", hotel: "Carlton", nom: "Ando", adresse: "23 rue de l’Indépendance",contact: "0345678901",}},
+      {id: 'p2', personnel: { id: 1, date: "7h - 15h", hotel: "Carlton", nom: "Ando", adresse: "23 rue de l’Indépendance",contact: "0345678901",}},
+      {id: 'p3', personnel: { id: 3, date: "7h - 15h", hotel: "Carlton", nom: "Lova", adresse: "23 rue de l’Indépendance",contact: "0345678901",}},
+      {id: 'p4', personnel: { id: 4, date: "7h - 15h", hotel: "Carlton", nom: "koko", adresse: "23 rue de l’Indépendance",contact: "0345678901",}},
+      {id: 'p5', personnel: ""},
+      {id: 'p6', personnel: ""},
+      {id: 'p7', personnel: ""},
+      {id: 'p8', personnel: ""},
+      {id: 'p9', personnel: ""},
+      {id: 'p10', personnel: ""},
+      {id: 'p11', personnel: ""},
+      {id: 'p12', personnel: ""},
+      {id: 'p13', personnel: ""},
+      {id: 'p14', personnel: ""},
+      {id: 'p15', personnel: ""},
+      {id: 'p16', personnel: ""},
+      {id: 'p17', personnel: ""},
+      {id: 'p18', personnel: ""},
+      {id: 'p19', personnel: ""},
+      {id: 'p20', personnel: ""},
+      {id: 'p21', personnel: ""},
+      {id: 'p22', personnel: ""},
+    ],
   };
+  
+  Car.headerDisplay(dataCar);
+  Car.DisplayContent(dataCar);
+  
+  Gestion.addPersonelManual(dataCar);
 
-  Car.headerDisplay(dataInCar);
-  Car.DisplayContent(dataInCar);
   Car.carousel();
   Car.active();
 
+  Gestion.generateTablePersonnel(data);
   Gestion.autoGeneratePersonnel(data);
-  Gestion.addPersonelManual();
   Gestion.autoDistribution();
 });
 
@@ -188,19 +189,16 @@ const Gestion = {
 
     data.forEach((element) => {
       const html = `
-                <div class="position-relative rounded-4 p-3 mb-2" data-id="${element.id}">
-                    <label for="listPersonnel_${element.id}">
-                        <h3 class="m-0 mb-2 p-0">${element.nom}</h3>
-                        <p class="opacity-50 mb-4">${element.adresse}</p>
-                        <span class="d-flex align-items-center gap-2">
-                            <p class="m-0 text-secondary fw-bold p-2 rounded-3" style="background: #F4F9FD;">106
-                            </p>
-                            <i class="bi bi-arrow-up fs-2 m-2 color-yellow"></i>
-                        </span>
-                    </label>
-                </div>
-            `;
-
+        <div class="position-relative rounded-4 p-3 mb-2" data-id="${element.id}">
+            <h3 class="m-0 mb-2 p-0">${element.nom}</h3>
+            <p class="opacity-50 mb-4">${element.adresse}</p>
+            <span class="d-flex align-items-center gap-2">
+                <p class="m-0 text-secondary fw-bold p-2 rounded-3" style="background: #F4F9FD;">106
+                </p>
+                <i class="bi bi-arrow-up fs-2 m-2 color-yellow"></i>
+            </span>
+        </div>
+        `;
       dataTable.push({ card: html });
       $tablePersonel.bootstrapTable("load", dataTable);
 
@@ -218,12 +216,44 @@ const Gestion = {
     });
   },
 
-  addPersonelManual: function () {
-    $("body").on("click", ".card-add-personel", function () {
+  addPersonelManual: function(car) {
+    $("body").on("click", ".card-add-personel", function (e) {
       $this = $(this);
-      $target = $this.data("target");
+      let newElement = {};
+      const target = $this.data("target");
+      const idBlock = $this.data('id-block');
+      $(target).modal("show");
+      
+      $('#table-list-personnel').on('check.bs.table', function(e,row, $element){
+        newElement = row        
+        const parent = $element.closest('tr');
 
-      $($target).modal("show");
+        $('.add-maual-personnel').on('click', function() {
+          const personnel = car.data.find(item => item.id == idBlock);
+
+          if(personnel){
+            personnel.personnel = newElement
+          }
+
+          $this.html(`
+              <div class="card rounded-4 p-3 w-100" style="min-height: 140px;">
+                  <h3 class="m-0 mb-2 p-0">${newElement.nom}</h3>
+                  <p class="opacity-50">${newElement.adresse}</p>
+                  <span class="d-flex align-items-center gap-1">
+                      <p class="m-0 text-secondary fw-bold p-2 rounded-3"
+                          style="background: #F4F9FD;">106
+                      </p>
+                      <i class="bi bi-arrow-up fs-4 color-yellow"></i>
+                  </span>
+              </div>
+          `)
+      
+
+          $(target).modal('hide');      
+          parent.addClass('use')          
+        })
+      })
+        
     });
   },
 
@@ -234,7 +264,35 @@ const Gestion = {
 
       $($target).modal("show");
     });
+
+
+    $('#table-personnel').on('check.bs.table', function() {
+      $.confirm({
+        title: 'Vous voulez ajoutez cette personne dans le voiture?',
+        content: 'Confirmer',
+        buttons: {
+            confirm: {
+              text: 'oui',
+              btnClass: 'btn-primary',
+              action: function(){
+                // $.alert('confirmer');
+              }
+            },
+            cancel: {
+                text: 'Non',
+                btnClass: 'btn-secondary',
+                action: function(){
+                    // $.alert('non');
+                }
+            }
+        }
+    });
+    })
   },
+  
+  generateTablePersonnel: function(data) {
+    $('.table-list-personnel').bootstrapTable('load', data)
+  }
 };
 
 const Car = {
@@ -243,34 +301,34 @@ const Car = {
       dots: false,
       infinite: false,
       speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 4,
+      // slidesToShow: 4,
+      // slidesToScroll: 4,
 
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true,
-          },
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-          },
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
-        },
-      ],
+      // responsive: [
+      //   {
+      //     breakpoint: 1024,
+      //     settings: {
+      //       slidesToShow: 3,
+      //       slidesToScroll: 3,
+      //       infinite: true,
+      //       dots: true,
+      //     },
+      //   },
+      //   {
+      //     breakpoint: 600,
+      //     settings: {
+      //       slidesToShow: 2,
+      //       slidesToScroll: 2,
+      //     },
+      //   },
+      //   {
+      //     breakpoint: 480,
+      //     settings: {
+      //       slidesToShow: 1,
+      //       slidesToScroll: 1,
+      //     },
+      //   },
+      // ],
     });
   },
 
@@ -285,7 +343,6 @@ const Car = {
 
   headerDisplay: function (car) {
     const cars = $("#cars");
-    const carContents = $("#cars-contents");
     const htmlCar = `
         <div class="col-12 col-md-3 mb-3" role="presentation">
             <button class="nav-link active bg-white rounded-5 p-1 w-100"
@@ -293,7 +350,7 @@ const Car = {
                 data-bs-target="#v-pills-voiture1" type="button" role="tab"
                 aria-controls="v-pills-voiture1" aria-selected="true">
 
-                <div class="p-2 px-3 rounded-5  card-car text-dark"
+                <div class="p-2 px-3 rounded-5  card-car text-dark active-car"
                     style="background: #F4F9FD;">
                     <h4 class="m-0">${car.voiture}</h4>
                     <p>${car.trajet}</p>
@@ -308,67 +365,104 @@ const Car = {
   DisplayContent: function (car) {
     const carContents = $("#cars-contents");
 
-    let passage = "";
-    console.log(car.data.passage);
-    {car.data.passage.forEach((el) => {
-      let htmlPersonnel = '';
-      if(el.personnel == ''){
-        htmlPersonnel =`
-        <div class="col-12 col-md-3 mb-3 card-add-personel " data-toggle="modal"
-            data-target="#add-personel" data-id=${el.key}>
-            <div class=" card rounded-4 p-3 w-100 d-flex justify-content-center align-items-center border-2 bg-transparent"
-              style="min-height: 140px;  border-style:dashed;">
-              <i class="bi me-2 bi-plus-lg fs-3 opacity-50"></i>
-              <h4 class="m-0 mb-2 p-0 opacity-50">Ajout mauel</h4>
+    const driver = this.getDriver(car.data);
+    const passages = this.getPassagers(car.data);
+
+    carContents.append(`
+      <div class="tab-pane fade show active" id="v-pills-voiture1" role="tabpanel" aria-labelledby="v-pills-voiture1-tab">
+          <div class="row">
+              ${driver}
+              ${passages}
           </div>
-        </div>`
+      </div>            
+    `);
+  },
+
+  getPassagers : function(data){
+    let passages = data.filter(item => item.id != 'c');
+
+    let cards = '';
+    {passages.forEach((passage) => {
+      if(passage.personnel == ''){
+        htmlPersonnel = Templante.default(passage);
       }else{
-        htmlPersonnel =  `
+        htmlPersonnel =  Templante.personel(passage);
+      }
+        cards += htmlPersonnel;
+      })
+      return cards;
+    };
+  },
+
+  getDriver : function(data){
+    const driver = data.find(item => item.id == 'c')?.personnel;
+    if(driver == ''){
+      html =Templante.default(driver.id);
+    }else{
+      html = Templante.driver(driver);
+    }
+    return html;
+  }
+};
+
+const Templante =  {
+  default : function(data){
+    const html = `
+      <div class="col-12 col-md-3 mb-3 card-add-personel " data-toggle="modal"
+          data-target="#add-personel" data-id-block=${data.id}>
+          
+          <div class=" card rounded-4 p-3 w-100 d-flex justify-content-center align-items-center border-2 bg-transparent"
+            style="min-height: 140px;  border-style:dashed;">
+            <i class="bi me-2 bi-plus-lg fs-3 opacity-50"></i>
+            <h4 class="m-0 mb-2 p-0 opacity-50">Ajout mauel</h4>
+        </div>
+
+      </div>
+    `
+    return html;
+  },
+
+  personel : function(data){
+    const html =`
         <div class="col-12 col-md-3 mb-3 card-add-personel" data-toggle="modal"
-            data-target="#add-personel" data-id=${el.key}>
+            data-target="#add-personel" data-id-block=${data.id}>
+            
             <div class="card rounded-4 p-3 w-100" style="min-height: 140px;">
-                <h3 class="m-0 mb-2 p-0">${el.personnel.nom}</h3>
-                <p class="opacity-50">12 rue des Fleurs</p>
+                <h3 class="m-0 mb-2 p-0">${data.personnel.nom}</h3>
+                <p class="opacity-50">${data.personnel.adresse}</p>
                 <span class="d-flex align-items-center gap-1">
                     <p class="m-0 text-secondary fw-bold p-2 rounded-3"
                         style="background: #F4F9FD;">106
                     </p>
                     <i class="bi bi-arrow-up fs-4 color-yellow"></i>
                 </span>
-
             </div>
+
         </div>`
-      }
+    return html;
+  },
 
-
-      passage += htmlPersonnel;
-
-    })
-      
-    carContents.append(`
-        <div class="tab-pane fade show active" id="v-pills-voiture1" role="tabpanel" aria-labelledby="v-pills-voiture1-tab">
-            <div class="row">
-                <div class="col-12 col-md-6 card-add-personel mb-3" data-toggle="modal" data-target="#add-personel">
-                    <div class="card rounded-4 p-3 w-100 d-flex flex-row justify-content-between align-items-center" style="min-height: 140px;">
-                        <div class="">
-                            <h3 class="m-0 mb-2 p-0">${car.data.chauffeur.name}</h3>
-                            <p class="opacity-50">Chauffeur</p>
-                            <span class="d-flex align-items-center gap-1">
-                                <p class="m-0 text-secondary fw-bold p-2 rounded-3"
-                                    style="background: #F4F9FD;">106
-                                </p>
-                                <i class="bi bi-arrow-up fs-4 color-yellow"></i>
-                                <i class="bi bi-arrow-down fs-4 color-green"></i>
-                            </span>
-                        </div>
-                        <img src="./assets/image/volant.png" alt="" class="opacity-50"style="width: 100px; height: 100px;">
-                    </div>
-                </div>
-                
-                ${passage}
+  driver : function(driver){
+    const html = `
+    <div class="col-12 col-md-6 card-add-personel mb-3" data-toggle="modal" data-target="#add-personel">
+        <div class="card rounded-4 p-3 w-100 d-flex flex-row justify-content-between align-items-center" style="min-height: 140px;">
+            <div class="">
+              <h3 class="m-0 mb-2 p-0">${driver.name}</h3>
+              <p class="opacity-50">Chauffeur</p>
+              <span class="d-flex align-items-center gap-1">
+                  <p class="m-0 text-secondary fw-bold p-2 rounded-3"
+                      style="background: #F4F9FD;">106
+                  </p>
+                  <i class="bi bi-arrow-up fs-4 color-yellow"></i>
+                  <i class="bi bi-arrow-down fs-4 color-green"></i>
+                </span>
             </div>
-        </div>            
-      `);
-    };
+            <img src="./assets/image/volant.png" alt="" class="opacity-50"style="width: 100px; height: 100px;">
+        </div>
+      </div>`
+    return html;
   }
-};
+
+
+}
+
